df98ec121aacc1d5544d9538286551788a17604e
AlekseyK <aleksey.kabishau@gmail.com>
2019-05-28 22:18:51 -0400

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Aleksey on 0528..19.
+//  Copyright © 2019 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

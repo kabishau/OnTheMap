4fa1da9e1e5742bb7b6164e2df85b53087839f81
Aleksey DreamLine <aleksey.kabishau@dreamline.com>
2019-06-12 17:13:17 -0400

    basic customization for tab bar items + icons in advance

--- OnTheMap/AppDelegate.swift
+++ OnTheMap/AppDelegate.swift
@@ -7,7 +7,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UITabBar.appearance().tintColor = UIColor(red: 38/255, green: 94/255, blue: 150/255, alpha: 1)
+        UITabBar.appearance().unselectedItemTintColor = UIColor(red: 38/255, green: 94/255, blue: 150/255, alpha: 0.5)
         return true
     }
 }
